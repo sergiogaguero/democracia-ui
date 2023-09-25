eafed08da79104db53648855339966abb5219c04
saguero <sergiogaguero@gmail.com>
2023-09-25 19:03:35 -0300

    remove user

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -18,18 +18,18 @@ export default function Profile( {user}:Props) {
       <VStack bg="white" maxW="56em" m="auto" p="24px" spacing="24px" borderRadius="12px">
         <HStack w="100%" justifyContent="space-between">
           <Text fontSize="3em">Pagina de usuario</Text>
-          <img
+          {/* <img
                 src={user?.image ? user.image : "/images/default.png"}
                 className="max-h-36"
                 alt={`profile photo of ${user?.name || "user"}`}
-              />
+              /> */}
         </HStack>
           {!user ? (
             <Text>Loading...</Text>
           ) : (
             <HStack spacing="24px" alignItems="stretch" w="100%" justifyContent="space-between">
               <UserDataForm user={user}/>
-              <FiscalRegisterForm/>
+              {/* <FiscalRegisterForm/> */}
             </HStack>
           )}
       </VStack>
